74f8648ae36cb4c166c48ae535a830db6c2de1c8
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-12 21:23:44 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState } from "react";
 
-import AddUser from './components/a/UserForm/User/AddUser';
-import UsersList from './components/a/UserForm/User/UsersList';
+import AddUser from "./components/a/UserForm/User/AddUser";
+import UsersList from "./components/a/UserForm/User/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
-  
-  const AddUserHandler = (uName,uAge,uCollegeName) => {
-    setUsersList((prevUsersList) =>{
-      return [...prevUsersList,{name:uName, age:uAge,collegeName:uCollegeName, id:Math.random().toString()}]
+
+  const AddUserHandler = (uName, uAge, uCollegeName) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          name: uName,
+          age: uAge,
+          collegeName: uCollegeName,
+          id: Math.random().toString(),
+        },
+      ];
     });
   };
 
   return (
     <Fragment>
-      <AddUser onAddUser ={AddUserHandler} />
-      <UsersList users ={usersList}/>
+      <AddUser onAddUser={AddUserHandler} />
+      <UsersList users={usersList} />
     </Fragment>
   );
 }
